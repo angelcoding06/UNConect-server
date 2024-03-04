77c7e419a92b33d9f55bb2e651b75848b7b9118a
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-03-04 14:27:22 -0500

    Add Swagger initial config

--- post_ms/src/main.ts
+++ post_ms/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar títulos de documentación
+  const options = new DocumentBuilder()
+    .setTitle('MongoDB Posts REST API')
+    .setDescription('Micoservicio de Posts con MongoDB')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // La ruta en que se sirve la documentación
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
