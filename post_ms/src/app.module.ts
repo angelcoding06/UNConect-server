0a71888a3cfdbc866cf8808f026afe85f0dd1398
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-03-05 18:12:16 -0500

    POSTS - Add mongodb config and mongodb connection

--- post_ms/src/app.module.ts
+++ post_ms/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
 
+import { ConfigModule } from '@nestjs/config';
+import mongodbConfig from './shared/config/mongodb.config';
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [mongodbConfig],
+    }),
+    DatabaseModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- post_ms/src/database/database.module.ts
+++ post_ms/src/database/database.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.provider';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('mongodb.uri');
+        return {
+          retryAttempts: 1,
+          authSource: 'admin',
+          uri: uri,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
