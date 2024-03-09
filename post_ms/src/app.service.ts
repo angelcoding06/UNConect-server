e58f08ed2888e6c8086a21d71546333c4a50a9e9
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-03-09 13:49:03 -0500

    POST - Clean App service

--- post_ms/src/app.service.ts
+++ post_ms/src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config'; // Import ConfigService
 
 @Injectable()
 export class AppService {
-  // constructor(private configService: ConfigService) {} // Inject ConfigService
-
   getHello(): string {
-    // const databaseName = this.configService.get('MONGO_HOSTNAME'); // Access environment variable
-    return `Hello World! Database name is: `;
+    return `Hello World!`;
   }
 }
