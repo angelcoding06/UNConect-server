4f2c4afe80f5fa3bc04cbaf40a61c19fcc7b78b3
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-03-27 11:52:04 -0500

    MEDIA - Fix .env consts

--- media_ms/src/shared/config/mongodb.config.ts
+++ media_ms/src/shared/config/mongodb.config.ts
@@ -2,13 +2,13 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongodb', () => {
   const {
-    MONGO_PORT,
-    MONGO_HOSTNAME,
-    MONGO_DATABASE,
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
+    MONGO_PORT_MEDIA,
+    MONGO_HOSTNAME_MEDIA,
+    MONGO_DATABASE_MEDIA,
+    MONGO_USERNAME_MEDIA,
+    MONGO_PASSWORD_MEDIA,
   } = process.env;
-  // const uri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
-  const uri = 'mongodb://mediauser:password@localhost:27018/mediaDB';
+  const uri = `mongodb://${MONGO_USERNAME_MEDIA}:${MONGO_PASSWORD_MEDIA}@${MONGO_HOSTNAME_MEDIA}:${MONGO_PORT_MEDIA}/${MONGO_DATABASE_MEDIA}?retryWrites=true&w=majority`;
+  // const uri = 'mongodb://mediauser:password@localhost:27018/mediaDB';
   return { uri };
 });
