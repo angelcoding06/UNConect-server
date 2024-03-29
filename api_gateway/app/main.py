3557ea622293ab6a4667b4e430b0e735af8a6f37
MoNKy0112 <jomoncaleanoa@unal.edu.co>
2024-03-29 12:59:24 -0500

    AG - change in main.py

--- api_gateway/app/main.py
+++ api_gateway/app/main.py
@@ -1,3 +1,4 @@
+from app.auth_ms.auth.auth_schema import Query as AuthQuery
 import strawberry
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
@@ -7,7 +8,7 @@ from app.post_ms.likes.likes_schema import Query as LikeQuery
 from app.post_ms.likes.likes_schema import Mutation as LikeMutation
 from app.post_ms.comments.comments_schema import Query as CommentQuery
 from app.post_ms.comments.comments_schema import Mutation as CommentMutation
-
+from app.auth_ms.auth.auth_schema import Query as AuthQuery, Mutation as AuthMutation
 # TODO fix the urls
 
 
@@ -19,16 +20,15 @@ class HelloQuery:
 
 
 @strawberry.type
-class Query(HelloQuery, PostQuery, LikeQuery, CommentQuery):
+class Query(HelloQuery, PostQuery, LikeQuery, CommentQuery, AuthQuery):
     pass
 
 
 @strawberry.type
-class Mutation(PostMutation, LikeMutation, CommentMutation):
+class Mutation(PostMutation, LikeMutation, CommentMutation, AuthMutation):
     pass
 
 
-schema = strawberry.Schema(Query)
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema)
 
@@ -39,3 +39,10 @@ app.include_router(graphql_app, prefix="/graphql")
 @app.get("/")
 async def root():
     return {"message": "Hello World from Api gateway"}
+
+
+@strawberry.type
+class HelloQuery:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
