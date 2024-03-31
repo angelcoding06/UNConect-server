86e5f36c85d3ab97b6b8c669e195bde7d11aed5b
SebastianOG509 <seortizg@unal.edu.co>
2024-03-30 21:39:39 -0500

    connect db neo4j

--- user_ms/src/index.js
+++ user_ms/src/index.js
@@ -6,6 +6,26 @@ const userRoutes = require('./routes/user.js'); // Importa las rutas relacionada
 const friendsRoutes = require('./routes/friends.js'); // Importa las rutas relacionadas con los amigos
 const User = require('./models/usermodel'); // Importa el modelo de usuario
 const Friends = require('./models/friendsmodel'); // Importa el modelo de amigos
+const neo4j = require('neo4j-driver');
+require('dotenv').config();
+
+// Variables de entorno
+const { NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD } = process.env;
+
+// Crear una instancia del driver de Neo4j
+const driver = neo4j.driver(NEO4J_URI, neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD));
+
+// Crear una sesión de Neo4j
+async function connectAndQueryNeo4j() {
+  const session = driver.session();
+  try {
+    console.log('Consulta a Neo4j completada con éxito');
+  } catch (error) {
+    console.error('Error al ejecutar la consulta en Neo4j:', error);
+  } finally {
+    await session.close();
+  }
+}
 
 // Función asincrónica para autenticar la conexión a la base de datos y sincronizar los modelos
 async function authenticateDatabase() {
@@ -21,11 +41,14 @@ async function authenticateDatabase() {
 
 authenticateDatabase(); // Llama a la función para autenticar la conexión a la base de datos y sincronizar los modelos
 
+connectAndQueryNeo4j(); // Iniciar la conexión y consulta a Neo4j al iniciar la aplicación
+
+
 app.get('/', (req, res) => {
   res.send('UNConnect users'); // Ruta de inicio que muestra un mensaje simple
 });
 
-// Usa las rutas de usuarios en la ruta /users y las rutas de amigos en la ruta /friends
+// // Usa las rutas de usuarios en la ruta /users y las rutas de amigos en la ruta /friends
 app.use(express.json()); // Habilita el middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use('/users', userRoutes); // Usa las rutas de usuarios
 app.use('/friends', friendsRoutes); // Usa las rutas de amigos
