693690c46443cc5cde308c614d25f9ce11ff7c8e
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2024-04-12 10:21:24 -0500

    AG - Add persons validation example

--- api_gateway/app/group_ms/persons/persons_resolvers.py
+++ api_gateway/app/group_ms/persons/persons_resolvers.py
@@ -4,74 +4,80 @@ from strawberry.exceptions import GraphQLError
 from app.const import GROUP_MS_URL
 import typing
 import json
-
+from app.utils.verifyuser import verifyUser
 def get_person_group()-> PersonGroupResponse:
-		try:
-				response = requests.get(f"{GROUP_MS_URL}/groups/getPerson/")
-				print("RESPONSE :", response.status_code)
-				if response.status_code != 200:
-						raise GraphQLError(str(response.text))
-				json_response = response.text
-				json_response=json.loads(json_response)
-				users = []
-				for person in json_response:
-					users.append(PersonGroupClass(**person))
-				users=PersonGroupResponse(persons=users)
-				return users
-		except ValueError as error: # Bad format
-				raise GraphQLError(str(error))
-		except requests.RequestException as error:  # net errors
-				raise GraphQLError(f"Error al contactar la API REST: {error}")
-		except KeyError as error:  # Keys error
-				raise GraphQLError(f"Error al procesar la respuesta: {error}")
+    try:
+        response = requests.get(f"{GROUP_MS_URL}/groups/getPerson/")
+        print("RESPONSE :", response.status_code)
+        if response.status_code != 200:
+            raise GraphQLError(str(response.text))
+        json_response = response.text
+        json_response=json.loads(json_response)
+        users = []
+        for person in json_response:
+          users.append(PersonGroupClass(**person))
+        users=PersonGroupResponse(persons=users)
+        return users
+    except ValueError as error: # Bad format
+        raise GraphQLError(str(error))
+    except requests.RequestException as error:  # net errors
+        raise GraphQLError(f"Error al contactar la API REST: {error}")
+    except KeyError as error:  # Keys error
+        raise GraphQLError(f"Error al procesar la respuesta: {error}")
 
-def create_person_group(user_id:str) -> PersonGroupClass:
-		try:
-				response = requests.post(f"{GROUP_MS_URL}/groups/postPerson/", json = {"user_id":user_id})
-				print(response)
-				print(response.text)
-				print(response.status_code)
+def create_person_group(token:str) -> PersonGroupClass:
+    userVerified = verifyUser(token)
+    if userVerified == "UNAUTHORIZED":
+        raise GraphQLError("UNAUTHORIZED")
+    if userVerified == "Fallo al verificar":
+        raise GraphQLError("Fallo al verificar")
+    user_id = userVerified.id
+    try:
+        response = requests.post(f"{GROUP_MS_URL}/groups/postPerson/", json = {"user_id":user_id})
+        print(response)
+        print(response.text)
+        print(response.status_code)
 
-				if response.status_code == 404:
-						raise GraphQLError(str(response.text))
-				text=response.text
-				text=json.loads(text)
-				return PersonGroupClass(**text)
-		except ValueError as error: # Bad format
-				raise GraphQLError(str(error))
-		except requests.RequestException as error:  # net errors
-				raise GraphQLError(f"Error al contactar la API REST: {error}")
-		except KeyError as error:  # Keys error
-				raise GraphQLError(f"Error al procesar la respuesta: {error}")
+        if response.status_code == 404:
+            raise GraphQLError(str(response.text))
+        text=response.text
+        text=json.loads(text)
+        return PersonGroupClass(**text)
+    except ValueError as error: # Bad format
+        raise GraphQLError(str(error))
+    except requests.RequestException as error:  # net errors
+        raise GraphQLError(f"Error al contactar la API REST: {error}")
+    except KeyError as error:  # Keys error
+        raise GraphQLError(f"Error al procesar la respuesta: {error}")
 
 def edit_person_group(user_id:str,id:int)-> PersonGroupClass:
-		try:
-				response = requests.put(f"{GROUP_MS_URL}/groups/putPerson/{id}/", json = {"user_id":user_id})
-				if response.status_code != 200:
-						raise GraphQLError((response.text))
-				text=response.text
-				text=json.loads(text)
-				return PersonGroupClass(**text)
+    try:
+        response = requests.put(f"{GROUP_MS_URL}/groups/putPerson/{id}/", json = {"user_id":user_id})
+        if response.status_code != 200:
+            raise GraphQLError((response.text))
+        text=response.text
+        text=json.loads(text)
+        return PersonGroupClass(**text)
 
-		except ValueError as error: # Bad format
-				raise GraphQLError(str(error))
-		except requests.RequestException as error:  # net errors
-				raise GraphQLError(f"Error al contactar la API REST: {error}")
-		except KeyError as error:  # Keys error
-				raise GraphQLError(f"Error al procesar la respuesta: {error}")
+    except ValueError as error: # Bad format
+        raise GraphQLError(str(error))
+    except requests.RequestException as error:  # net errors
+        raise GraphQLError(f"Error al contactar la API REST: {error}")
+    except KeyError as error:  # Keys error
+        raise GraphQLError(f"Error al procesar la respuesta: {error}")
 
 def delete_person_group(id:int) -> str:
-		try:
-				response = requests.delete(f"{GROUP_MS_URL}/groups/deletePerson/{id}")
-				if response.status_code != 200:
-					raise GraphQLError(str(response.text))
-				text=response.json()
-				return text["message"]
+    try:
+        response = requests.delete(f"{GROUP_MS_URL}/groups/deletePerson/{id}")
+        if response.status_code != 200:
+          raise GraphQLError(str(response.text))
+        text=response.json()
+        return text["message"]
 
-		except ValueError as error: # Bad format
-				raise GraphQLError(str(error))
-		except requests.RequestException as error:  # net errors
-				raise GraphQLError(f"Error al contactar la API REST: {error}")
-		except KeyError as error:  # Keys error
-				raise GraphQLError(f"Error al procesar la respuesta: {error}")
+    except ValueError as error: # Bad format
+        raise GraphQLError(str(error))
+    except requests.RequestException as error:  # net errors
+        raise GraphQLError(f"Error al contactar la API REST: {error}")
+    except KeyError as error:  # Keys error
+        raise GraphQLError(f"Error al procesar la respuesta: {error}")
 
